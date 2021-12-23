7e76739b8102bf2076c0dcff774d8854e5810dcb
Marc Picas <marc.picash@gmail.com>
2021-12-23 19:19:27 +0100

    Ejercicio 3

---
+++ OpenBootcamp_Spring_Ejercicio3/src/main/java/com/openbootcamp/spring/ejercicio3/OpenBootcampSpringEjercicio3Application.java
@@ -0,0 +1,16 @@
+package com.openbootcamp.spring.ejercicio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBootcampSpringEjercicio3Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OpenBootcampSpringEjercicio3Application.class, args);
+
+		System.out.println("hola");
+	}
+
+}
---
+++ OpenBootcamp_Spring_Ejercicio3/src/test/java/com/openbootcamp/spring/ejercicio3/OpenBootcampSpringEjercicio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbootcamp.spring.ejercicio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBootcampSpringEjercicio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
